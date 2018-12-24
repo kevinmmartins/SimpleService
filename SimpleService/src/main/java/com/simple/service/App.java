a53854829114a94fe535a610de1b247c6c4ded9d
Kevin Martins <kevinmmartins@gmail.com>
2018-12-24 12:14:11 -0200

    Fix build problem

--- SimpleService/src/main/java/com/simple/service/App.java
+++ SimpleService/src/main/java/com/simple/service/App.java
@@ -2,9 +2,8 @@ package com.simple.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
